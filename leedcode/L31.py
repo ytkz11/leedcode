4b4bd9afddca30a54b8e64ac2008062c945b5795
abner11Ytkz <ytkz11@163.com>
2021-05-31 02:16:01 +0800

    leetcode

---
+++ leedcode/L31.py
@@ -0,0 +1,14 @@
+class Solution:
+    def nextPermutation(self, nums) -> None:
+        """
+        Do not return anything, modify nums in-place instead.
+        """
+        for i in nums:
+
+
+
+if __name__ == '__main__':
+    nums = [1, 2, 3]
+    A = Solution()
+    a = A.nextPermutation(nums)
+    a = 0
