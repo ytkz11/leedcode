0fc4636ea760b8610a18d7599ab13b2aa2a75a34
ytkz11 <ytkz11@163.com>
2024-03-08 15:26:40 +0800

    猜数字游戏

---
+++ leedcode/easy/x的平方根.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*- 
+# @Time : 2024/3/8 15:02 
+# @File : x的平方根.py 
+from typing import List
+#给你一个非负整数 x ，计算并返回 x 的 算术平方根 。
+
+class Solution:
+    def function(self, nums) -> None:
+        """
+        Do not return anything, modify nums in-place instead.
+        """
+        a = 0
+
+
+if __name__ == '__main__':
+    nums = 4
+    A = Solution()
+    a = A.function(nums)
+    print(a)
